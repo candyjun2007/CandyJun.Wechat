d1f18a8aebc9ae6fd24afee5d44420e409d9c661
刘俊 <liujun6@zhongan.com.cn>
2017-09-29 17:36:25 +0800

    添加项目文件。

---
+++ CandyJun.Wechat/Startup.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Senparc.Weixin.Entities;
+using Senparc.Weixin.MP.Containers;
+using Microsoft.Extensions.Options;
+using CandyJun.Wechat.Utilities;
+
+namespace CandyJun.Wechat
+{
+    public class Startup
+    {
+        //public Startup(IConfiguration configuration)
+        //{
+        //    Configuration = configuration;
+        //}
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+
+            //Senparc.Weixin.SDK 配置
+            builder.AddJsonFile("SenparcWeixin.json", optional: true);
+
+            Configuration = builder.Build();
+            //提供网站根目录
+            Server.AppDomainAppPath = env.ContentRootPath;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            services.Configure<SenparcWeixinSetting>(Configuration.GetSection("SenparcWeixinSetting"));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env,
+            IOptions<SenparcWeixinSetting> senparcWeixinSetting)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseBrowserLink();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller}/{action=Index}/{id?}");
+            });
+
+            #region 微信相关
+
+            //注册微信
+            AccessTokenContainer.Register(senparcWeixinSetting.Value.WeixinAppId, senparcWeixinSetting.Value.WeixinAppSecret);
+
+            //Senparc.Weixin SDK 配置
+            Senparc.Weixin.Config.DefaultSenparcWeixinSetting = senparcWeixinSetting.Value;
+
+            #endregion
+        }
+    }
+}
---
+++ CandyJun.Wechat/Utilities/Server.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CandyJun.Wechat.Utilities
+{
+    public static class Server
+    {
+        private static string _appDomainAppPath;
+        public static string AppDomainAppPath
+        {
+            get
+            {
+                if (_appDomainAppPath == null)
+                {
+#if NET45
+                    _appDomainAppPath = HttpRuntime.AppDomainAppPath;
+#else
+                    _appDomainAppPath = AppContext.BaseDirectory; //dll所在目录：;
+#endif
+                }
+                return _appDomainAppPath;
+            }
+            set
+            {
+                _appDomainAppPath = value;
+#if NETSTANDARD1_6 || NETSTANDARD2_0
+                if (!_appDomainAppPath.EndsWith("\\"))
+                {
+                    _appDomainAppPath += "\\";
+                }
+#endif
+            }
+        }
+
+        public static string GetMapPath(string virtualPath)
+        {
+            if (virtualPath == null)
+            {
+                return "";
+            }
+            else if (virtualPath.StartsWith("~/"))
+            {
+                return virtualPath.Replace("~/", AppDomainAppPath).Replace("/", "\\");
+            }
+            else
+            {
+                return Path.Combine(AppDomainAppPath, virtualPath.Replace("/", "\\"));
+            }
+        }
+
+        public static HttpContext HttpContext
+        {
+            get
+            {
+                HttpContext context = new DefaultHttpContext();
+                return context;
+            }
+        }
+    }
+}
